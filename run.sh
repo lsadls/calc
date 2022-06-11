03cbcec2203c72f99d2d441a38bb0ec87e8c6380
lsadls <106911889+lsadls@users.noreply.github.com>
2022-06-11 15:55:39 +0000

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 wget -O /tmp/trojan-go.zip https://github.com/p4gefau1t/trojan-go/releases/latest/download/trojan-go-linux-amd64.zip
-unzip /tmp/trojan-go.zip -d /root
-rm -r /tmp/trojan-go.zip /root/example /root/geoip.dat
-chmod 0755 /root/trojan-go
-mv /root/trojan-go /root/calc
+unzip /tmp/trojan-go.zip -d /tmp
+mv /tmp/trojan-go /root/calc
+rm -r /tmp/*
+chmod 0755 /root/calc
 
 cat << EOF > /root/server.yaml
 run-type: server
 local-addr: 0.0.0.0
-local-port: 8080
+local-port: $PORT
 remote-addr: example.com
 remote-port: 80
 log-level: 5
@@ -20,13 +20,6 @@ websocket:
 transport-plugin:
     enabled: true
     type: plaintext
-router:
-  enabled: true
-  block:
-    - 'geoip:private'
-    - 'geosite:category-ads'
-  geoip: /root/geoip-only-cn-private.dat
-  geosite: /root/geosite.dat
 EOF
 
 /root/calc -config /root/server.yaml
